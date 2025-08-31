5e2b505dbd7f2319ff4846fbae05c5ee559afee0
RoyAWilson <royawilson@hotmail.co.uk>
2025-08-31 16:15:46 +0100

    Links to description page working need to figure out why pictures not showing up.

--- my_site/roys_site/my_cv/urls.py
+++ my_site/roys_site/my_cv/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.cv_page, name="cv_page"),
+    # path("job_desc_page/", views.job_desc_page, name="job_desc_page")
+    path("<slug:slug>", views.job_desc_page, name="job_desc_page")
 ]
--- my_site/roys_site/my_cv/views.py
+++ my_site/roys_site/my_cv/views.py
@@ -6,3 +6,7 @@ from .models import Jobs
 def cv_page(request):
     job = Jobs.objects.all().order_by("job_title")
     return render(request, "cv_page.html", {"jobs": job})
+
+def job_desc_page(request, slug):
+    job = Jobs.objects.get(slug=slug)
+    return render(request, "job_desc_page.html", {"job": job})
