cdf33cd4c31970397b70a718e6b3d63f70b2d5c7
RoyAWilson <royawilson@hotmail.co.uk>
2025-09-02 18:59:52 +0100

    Add database to portfolio app to hold the details of portfolios and pictures.  Stil needs some work and needs to be linked to template page.

--- my_site/roys_site/my_portfolio/models.py
+++ my_site/roys_site/my_portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Portfolio(models.Model):
+    project_title = models.CharField(max_length=150)
+    project_img = models.ImageField(blank=True)
+    Project_desc = models.TextField()
+    date_completed = models.DateField()
+
+    def __str__(self):
+        return self.project_title
+    
