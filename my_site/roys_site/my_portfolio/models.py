3baf5c86a1b51a78f06ac6557a9bc51dc4baff41
RoyAWilson <royawilson@hotmail.co.uk>
2025-09-03 12:01:14 +0100

    Updated database Portfolio with URL field and slug field required for further development of portfolio area.

--- my_site/roys_site/my_portfolio/models.py
+++ my_site/roys_site/my_portfolio/models.py
@@ -6,6 +6,8 @@ class Portfolio(models.Model):
     project_img = models.ImageField(blank=True)
     Project_desc = models.TextField()
     date_completed = models.DateField()
+    project_url = models.URLField(max_length=200, default="Enter URL here")
+    project_slug = models.SlugField(default="abcdef")
 
     def __str__(self):
         return self.project_title
